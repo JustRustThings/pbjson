6bbb0dfff18244267934206c1bee425af12c0a11
roblabla <unfiltered@roblab.la>
2023-02-13 12:14:49 +0100

    Replace chrono with time 0.3

--- pbjson-types/src/timestamp.rs
+++ pbjson-types/src/timestamp.rs
@@ -1,24 +1,25 @@
 use crate::Timestamp;
-use chrono::{DateTime, NaiveDateTime, Utc};
 use serde::de::Visitor;
 use serde::Serialize;
+use time::format_description::well_known::Rfc3339;
 
-impl TryFrom<Timestamp> for chrono::DateTime<Utc> {
-    type Error = std::num::TryFromIntError;
-    fn try_from(value: Timestamp) -> Result<Self, Self::Error> {
-        let Timestamp { seconds, nanos } = value;
-
-        let dt = NaiveDateTime::from_timestamp(seconds, nanos.try_into()?);
-        Ok(Self::from_utc(dt, Utc))
+impl From<time::OffsetDateTime> for Timestamp {
+    fn from(dt: time::OffsetDateTime) -> Self {
+        // The UNIX timestamp is relative to UTC by definition, the time crate respects this.
+        let seconds = dt.unix_timestamp();
+        // `.nanoseconds()` guarantees a return value in 0 .. 1_000_000_000 and so will
+        // always fit in an `i32`.
+        let nanos = dt.nanosecond() as i32;
+        Self { seconds, nanos }
     }
 }
 
-impl From<DateTime<Utc>> for Timestamp {
-    fn from(value: DateTime<Utc>) -> Self {
-        Self {
-            seconds: value.timestamp(),
-            nanos: value.timestamp_subsec_nanos() as i32,
-        }
+impl From<Timestamp> for time::OffsetDateTime {
+    fn from(ts: Timestamp) -> Self {
+        let ts = ts.seconds as i128 * 1_000_000_000 + ts.nanos as i128;
+        // This cannot fail since the passed value is supposed to be a
+        // valid UTC timestamp itself.
+        Self::from_unix_timestamp_nanos(ts).unwrap()
     }
 }
 
@@ -27,8 +28,8 @@ impl Serialize for Timestamp {
     where
         S: serde::Serializer,
     {
-        let t: DateTime<Utc> = self.clone().try_into().map_err(serde::ser::Error::custom)?;
-        serializer.serialize_str(t.to_rfc3339().as_str())
+        let t: time::OffsetDateTime = self.clone().try_into().map_err(serde::ser::Error::custom)?;
+        serializer.serialize_str(&t.format(&Rfc3339).map_err(serde::ser::Error::custom)?)
     }
 }
 
@@ -45,8 +46,7 @@ impl<'de> Visitor<'de> for TimestampVisitor {
     where
         E: serde::de::Error,
     {
-        let d = DateTime::parse_from_rfc3339(s).map_err(serde::de::Error::custom)?;
-        let d: DateTime<Utc> = d.into();
+        let d = time::OffsetDateTime::parse(s, &Rfc3339).map_err(serde::de::Error::custom)?;
         Ok(d.into())
     }
 }
@@ -81,16 +81,6 @@ mod tests {
 
     #[test]
     fn test_date() {
-        let datetime = FixedOffset::east(5 * 3600)
-            .ymd(2016, 11, 8)
-            .and_hms(21, 7, 9);
-        let encoded = datetime.to_rfc3339();
-        assert_eq!(&encoded, "2016-11-08T21:07:09+05:00");
-
-        let utc: DateTime<Utc> = datetime.into();
-        let utc_encoded = utc.to_rfc3339();
-        assert_eq!(&utc_encoded, "2016-11-08T16:07:09+00:00");
-
         let deserializer = BorrowedStrDeserializer::<'_, Error>::new(&encoded);
         let a: Timestamp = Timestamp::deserialize(deserializer).unwrap();
         assert_eq!(a.seconds, utc.timestamp());
